d27a49ee648338bd3668a63e5045ad8889d372ec
speakingincode <brian@speakingincode.net>
2020-09-27 12:18:04 -0700

    fix iOS warnigns

--- beta.sh
+++ beta.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+function clean() {
+  flutter clean
+}
+
 function unit_test() {
   flutter test
 }
@@ -11,27 +15,32 @@ function screenshots() {
 }
 
 function bump_version() {
-  echo
-  #(cd android && bundle exec fastlane bump_version)
-  #(cd android && bundle exec fastlane make_changelog)
+  echo "Updating version and changelog."
+  (cd android && bundle exec fastlane bump_version)
+  (cd android && bundle exec fastlane make_changelog)
 }
 
 function build_releases() {
-  echo
-  #flutter build appbundle --release
-  #flutter build ios --release
+  echo "Building for iOS"
+  flutter build ios --release
+  echo "Building for Android"
+  flutter build appbundle --release
 }
 
 function upload_android_beta() {
+  echo "Uploading to Android beta track"
   (cd android && bundle exec fastlane beta)
 }
 
 function upload_ios_beta() {
-  echo
+  echo "Uploading to iOS beta track"
+  (cd ios && bundle exec fastlane beta)
 }
 
+clean
 #unit_test
 #screenshots
-#bump_version
-#build_releases
+bump_version
+build_releases
 upload_android_beta
+upload_ios_beta
