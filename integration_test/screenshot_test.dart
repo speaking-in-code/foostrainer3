4bcb40e3473f67ba17e1851e619052624a56dc69
Brian Eaton <brian@speakingincode.net>
2024-01-14 19:24:55 -0800

    Initialize the flutter bindings so the test will work

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -1,15 +1,20 @@
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
 import 'package:fbtl_screenshots/fbtl_screenshots.dart';
 
 import 'app_starter.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   //final _screenshots = FBTLScreenshots();
   //_screenshots.connect();
 
   testWidgets('Home Screen', (WidgetTester tester) async {
     await startFoosTrainer(tester);
+    print('Home Screen test finished');
     //await _screenshots.takeScreenshot(tester, '01-home-screen');
   });
 }
