6205905cd5fbd37278ec7fb6aaf93d3001914ebe
Brian Eaton <brian@speakingincode.net>
2024-01-21 12:35:40 -0800

    Add back home screen integration tests

--- integration_test/all_test.dart
+++ integration_test/all_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'app_starter.dart';
 import 'home_screen_test.dart';
 
 void main() {
-  // IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final appStarter = AppStarter();
 
-  group('home screen', homeScreenTests);
+  group('home screen', homeScreenTests(appStarter));
 }
--- integration_test/home_screen_test.dart
+++ integration_test/home_screen_test.dart
@@ -3,28 +3,30 @@ import 'package:ft3/keys.dart';
 
 import 'app_starter.dart';
 
-void homeScreenTests() {
-  testWidgets('renders home screen', (WidgetTester tester) async {
-    await startFoosTrainer(tester);
-    expect(find.text('Start Practice'), findsOneWidget);
-    expect(find.text('FoosTrainer'), findsOneWidget);
-    expect(find.text('Practice'), findsOneWidget);
-    expect(find.text('History'), findsOneWidget);
-    expect(find.text('Progress'), findsOneWidget);
-    expect(find.byKey(Keys.moreKey), findsOneWidget);
-  });
+Function() homeScreenTests(AppStarter appStarter) {
+  return () {
+    testWidgets('renders home screen', (WidgetTester tester) async {
+      await tester.pumpWidget(await appStarter.mainApp);
+      expect(find.text('Start Practice'), findsOneWidget);
+      expect(find.text('FoosTrainer'), findsOneWidget);
+      expect(find.text('Practice'), findsOneWidget);
+      expect(find.text('History'), findsOneWidget);
+      expect(find.text('Progress'), findsOneWidget);
+      expect(find.byKey(Keys.moreKey), findsOneWidget);
+    });
 
-  testWidgets('navigates to history', (WidgetTester tester) async {
-    await startFoosTrainer(tester);
-    await tester.tap(find.text('History'));
-    await tester.pumpAndSettle();
-    await expectLater(find.byKey(Keys.calendarDatePicker), findsOneWidget);
-  });
+    testWidgets('navigates to history', (WidgetTester tester) async {
+      await tester.pumpWidget(await appStarter.mainApp);
+      await tester.tap(find.text('History'));
+      await tester.pumpAndSettle();
+      await expectLater(find.byKey(Keys.calendarDatePicker), findsOneWidget);
+    });
 
-  testWidgets('navigates to progress', (WidgetTester tester) async {
-    await startFoosTrainer(tester);
-    await tester.tap(find.text('Progress'));
-    await tester.pumpAndSettle();
-    expect(find.text('All Drills'), findsOneWidget);
-  });
+    testWidgets('navigates to progress', (WidgetTester tester) async {
+      await tester.pumpWidget(await appStarter.mainApp);
+      await tester.tap(find.text('Progress'));
+      await tester.pumpAndSettle();
+      expect(find.text('All Drills'), findsOneWidget);
+    });
+  };
 }
